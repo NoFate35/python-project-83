da1173bff141ec295a1e100d12b71a73feb6860e
NoFate35 <vsokol35@mail.ru>
2025-04-30 10:50:42 +0300

    Changes to be committed: 	modified:   page_analyzer/__pycache__/__init__.cpython-311.pyc 	modified:   page_analyzer/__pycache__/app.cpython-311.pyc 	modified:   page_analyzer/app.py 	modified:   pyproject.toml 	modified:   uv.lock

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+print("тооооокеееееннр", app.config['SECRET_KEY'])
+
 
 @app.route('/')
 def get_index():
