b00235ffdbaab1340690caa2aac578aaeb217b09
NoFate35 <vsokol35@mail.ru>
2025-05-03 18:51:09 +0300

    Changes to be committed: 	modified:   page_analyzer/__pycache__/app.cpython-311.pyc 	modified:   page_analyzer/app.py 	new file:   page_analyzer/templates/index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 
@@ -9,10 +9,9 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-print("тооооокеееееннр", app.config['SECRET_KEY'])
 
 
 @app.route('/')
 def get_index():
-    return "Heeeeeellloooo"
+    return render_template('index.html')
     
