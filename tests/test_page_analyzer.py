4442c29f859bcd2e28703fbc21928477a0800224
NoFate35 <vsokol35@mail.ru>
2025-05-04 14:47:33 +0300

    Changes to be committed: 	modified:   pyproject.toml 	new file:   tests/__init__.py 	new file:   tests/test_page_analyzer.py 	modified:   uv.lock

---
+++ tests/test_page_analyzer.py
@@ -0,0 +1,25 @@
+import pytest
+from page_analyzer import app
+
+@pytest.fixture()
+def test_app():
+    test_app = app
+    test_app.config.update({
+        "TESTING": True,
+    })
+
+    yield test_app
+
+
+@pytest.fixture()
+def client(test_app):
+    return test_app.test_client()
+
+
+@pytest.fixture()
+def runner(test_app):
+    return test_app.test_cli_runner()
+
+def test_request_example(client):
+    response = client.get("/")
+    assert '<h1 class="display-3">Анализатор страниц</h1>' in response.text
