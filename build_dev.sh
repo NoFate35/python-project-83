30685c8b39d43ea83cac752079609562d86f0378
NoFate35 <vsokol35@mail.ru>
2025-05-07 20:28:59 +0300

    Changes to be committed: 	modified:   build_dev.sh

--- build_dev.sh
+++ build_dev.sh
@@ -1,11 +1,13 @@
-:'
-#!/bin/bash
-sudo service postgresql start
-psql -a -d $DATABASE_URL flaskdb
-psql py_flaskdb < database.sql
-'
-
+if (which -a bash == "/data/data/com.termux/files/usr/bin/bash")
+then
 #!/data/data/com.termux/files/usr/bin/bash
 pg_ctl -D $PREFIX/var/lib/postgresql start
 psql py_flaskdb < database.sql
+
+else
+#!/bin/bash
+sudo service postgresql start
+#psql -a -d $DATABASE_URL py_flaskdb
+psql py_flaskdb < database.sql
+fi
 
