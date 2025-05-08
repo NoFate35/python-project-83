02fc7b5812ea594af15d5b53c6b23e6b593272fd
NoFate35 <vsokol35@mail.ru>
2025-05-08 22:21:39 +0300

    Changes to be committed: 	modified:   .gitignore 	modified:   database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS urls (
+DROP TABLE IF EXISTS urls;
+CREATE TABLE urls (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar (255),
     created_at TIMESTAMP
