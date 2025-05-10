eb3d4892562f1a4d6d2f03e020670622cbf23dd1
NoFate35 <vsokol35@mail.ru>
2025-05-10 21:36:00 +0300

    Changes to be committed: 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
 make install && psql -a -d $DATABASE_URL -f database.sql
-make install
-make render-start
+make start
