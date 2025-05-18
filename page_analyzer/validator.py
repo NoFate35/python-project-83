b8c189ea9c3068d39c63f966d085b292cca1d50f
NoFate35 <vsokol35@mail.ru>
2025-05-18 21:05:23 +0300

    Changes to be committed: 	modified:   page_analyzer/validator.py

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -1,4 +1,4 @@
 from urllib.parse import urlparse
+from validators import url
 def validate(url):
-    o = urlparse("http://docs.python.org:80/3/library/urllib.parse.html?"
-             "highlight=params#url-parsing")
+    o = urlparse(url)
