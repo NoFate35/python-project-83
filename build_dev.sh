38647c2a6bedaa03c60c81aeabb00753839424f6
NoFate35 <vsokol35@mail.ru>
2025-05-18 20:45:09 +0300

    Changes to be committed: 	modified:   build_dev.sh 	deleted:    gunicorn.conf.py
    ADD functionality to dev in laptop

--- build_dev.sh
+++ build_dev.sh
@@ -1,5 +1,5 @@
 intdir=$(which bash)
-if echo $intdir == "/data/data/com.termux/files/usr/bin/bash"
+if [echo $intdir == "/data/data/com.termux/files/usr/bin/bash"]
 
 then
 #!/data/data/com.termux/files/usr/bin/bash
@@ -7,9 +7,9 @@ pg_ctl -D $PREFIX/var/lib/postgresql start
 psql py_flaskdb < database.sql
 
 else
-#!/bin/bash
+#!/usr/bin/bash
 sudo service postgresql start
-psql -a -d $DATABASE_URL py_flaskdb
+#psql -a -d $DATABASE_URL py_flaskdb
 psql py_flaskdb < database.sql
 
 fi
