cd705123c56f77463d834173f70d27a20889d6e0
NoFate35 <vsokol35@mail.ru>
2025-05-30 21:16:12 +0300

    Changes to be committed: 	modified:   page_analyzer/validator.py
    fix validator

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -7,7 +7,7 @@ def validate(try_url):
     print('try uuurl', try_url)
     url_object = urlparse(try_url)
     print('url_oojec', url_object)
-    normalize_url = url_object._replace(path="").geturl()
+    normalize_url = url_object._replace(path="", params="", query="", fragment="").geturl()
     if not url_validate(normalize_url):
         return False
     return normalize_url
