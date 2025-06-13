f4363c7cf3788992172776a17884ce9c51a6400a
NoFate35 <vsokol35@mail.ru>
2025-06-13 12:16:20 +0300

    Changes to be committed: 	modified:   build.sh
    	 TRY tu upload to Railways paas

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 export PATH="$HOME/.local/bin:$PATH"
+apt install mlocate
+updatedb
 locate psql
 make install && psql -a -d $DATABASE_URL -f database.sql
 make start
