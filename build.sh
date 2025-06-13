aea2fbd7bf14353a65fcb151804ea4c1c88276f7
NoFate35 <vsokol35@mail.ru>
2025-06-13 12:20:47 +0300

    Changes to be committed: 	modified:   build.sh
    	 TRY tu upload to Railways paas

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 export PATH="$HOME/.local/bin:$PATH"
-apt install mlocate
+apt-get install mlocate
 updatedb
 locate psql
 make install && psql -a -d $DATABASE_URL -f database.sql
