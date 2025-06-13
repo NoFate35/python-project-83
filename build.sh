c3e0d04efe4f2211aca04199575c0a5ede4a9dcc
NoFate35 <vsokol35@mail.ru>
2025-06-13 09:39:47 +0300

    Changes to be committed: 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
 apt update
+y
 apt install postgresql
 make install && psql -a -d $DATABASE_URL -f database.sql
 make start
