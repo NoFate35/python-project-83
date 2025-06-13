40ff2997bf9ce9cff564a32302b22d32360eda06
NoFate35 <vsokol35@mail.ru>
2025-06-13 08:34:22 +0300

    Changes to be committed: 	modified:   build.sh
    	TRY tu upload to Railways paas

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="/usr/local/pgsql/bin:$PATH"
+postgres --version
 make install && psql -a -d $DATABASE_URL -f database.sql
 make start
